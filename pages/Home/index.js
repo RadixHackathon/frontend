62db38ebdfe16f973aabaf8642907c6e8b70ac4c
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2023-03-23 00:30:23 +0530

    modal

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -5,6 +5,8 @@ import { useState } from 'react'
 import Router from 'next/router'
 export default function Home() {
   const [wallet, setWallet] = useState('')
+  const [modal, setModal] = useState(false)
+  const [modalType, setModalType] = useState('')
   const [pools, setPools] = useState([
     {
       name: 'Pool 1',
@@ -38,13 +40,19 @@ export default function Home() {
     },
   ])
   return (
-    <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+    <div
+      className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen"
+      // onClick={() => {
+      //   setModal(false)
+      // }}
+    >
       <Head>
         <title>AlkyneFi</title>
       </Head>
 
       <Header />
       <main className=" flex flex-1 w-full flex-col p-12 sm:mt-20 mt-20 background-gradient gap-16">
+        {modal && <ModalAmount setModal={setModal} modalType={modalType} />}
         <div className="flex flex-row justify-between">
           <div className="grid">
             <p className="text-xl">Wallet Address</p>
@@ -54,7 +62,11 @@ export default function Home() {
             <p className="text-xl">Balance</p>
             <p className="ml-5 text-xl leading-7 text-gray-500">$1000.00</p>
           </div>
-          <Investment amt="$1000.00" />
+          <Investment
+            amt="$1000.00"
+            setModal={setModal}
+            setModalType={setModalType}
+          />
         </div>
         <div>
           <h1 className="font-display text-5xl font-bold tracking-normal text-gray-300 ">
@@ -84,14 +96,34 @@ export default function Home() {
   )
 }
 
-function Investment({ amt }) {
+function Investment({ amt, setModal, setModalType }) {
   return (
-    <div className="bg-white text-black p-5 rounded-lg flex items-center justify-center opacity-80 hover:opacity-100 cursor-pointer" 
-    onClick={() => {Router.push('/Investment')}}  
-  >
+    <div className=" bg-white text-black p-5 rounded-lg flex flex-col gap-4 items-center justify-center opacity-80 hover:opacity-100 cursor-pointer">
       <div className="flex-row flex gap-4 items-center">
-        <p className="font-bold text-lg text-gray-900">Total Money Invested</p>
+        <p className="font-bold text-lg text-gray-900">
+          AlkyneFi Wallet Balance
+        </p>
         <p className="font-bold text-2xl text-gray-900">{amt}</p>
+      </div>
+      <div className="flex flex-row w-full justify-evenly">
+        <button
+          className="bg-blue-600 rounded-lg p-2 text-white"
+          onClick={() => {
+            setModal(true)
+            setModalType('Invest')
+          }}
+        >
+          Invest
+        </button>
+        <button
+          className="bg-blue-600 rounded-lg p-2 text-white"
+          onClick={() => {
+            setModal(true)
+            setModalType('Withdraw')
+          }}
+        >
+          Withdraw
+        </button>
       </div>
     </div>
   )
@@ -132,6 +164,55 @@ function Pools({
       >
         Invest
       </button>
+    </div>
+  )
+}
+
+function ModalAmount({ setModal, modalType }) {
+  const [value, setValue] = useState()
+  return (
+    <div
+      onClick={(e) => {
+        setModal(false)
+      }}
+      className="z-10 bg-gray-900 bg-opacity-80 fixed top-0 left-0 w-full text-black h-full flex justify-center items-center"
+    >
+      <div
+        onClick={(e) => {
+          e.stopPropagation()
+        }}
+        className="flex flex-col bg-white w-1/2 h-1/2 rounded-lg items-center justify-center"
+      >
+        <div className="flex flex-row justify-between items-center p-12 w-full">
+          <p className="text-2xl font-bold">{modalType} Amount</p>
+          <p
+            className="text-2xl cursor-pointer"
+            onClick={() => {
+              setModal(false)
+            }}
+          >
+            x
+          </p>
+        </div>
+        <div className="flex flex-col h-full gap-5">
+          <p className="text-2xl font-bold">Amount</p>
+          <input
+            type="text"
+            className="rounded-lg text-black p-1 border-2"
+            onChange={(e) => {
+              setValue(e.target.value)
+            }}
+          />
+          <button
+            className="bg-blue-600 rounded-lg p-2 text-white"
+            onClick={() => {
+              alert(value)
+            }}
+          >
+            {modalType}
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
