cc808f61bc4c9b3b16505ff034531dde1aa703e9
naman-ng <naman21171@iiitd.ac.in>
2023-03-22 20:13:43 +0530

    add connect button

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,31 +1,46 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from 'next/image';
+import Link from 'next/link';
+import { RadixDappToolkit } from '@radixdlt/radix-dapp-toolkit';
+import { useEffect } from 'react';
 
 export default function Header({ photo }) {
+  useEffect(() => {
+    const rdt = RadixDappToolkit(
+      {
+        dAppDefinitionAddress: 'account_tdx_b_1ppglnkmukh36l2dfw3uygvgjf2jsfypl885u9840md7swrvpmj',
+        dAppName: 'AlkyneFi',
+      },
+      (requestData) => {
+        requestData({
+          accounts: { quantifier: 'atLeast', quantity: 1 },
+        }).map(({ data: { accounts } }) => {
+          // set your application state
+        });
+      },
+      {
+        networkId: 11,
+        onDisconnect: () => {
+          // clear your application state
+        },
+        onInit: ({ accounts }) => {
+          // set your initial application state
+        },
+      }
+    );
+  }, []);
+
   return (
     <header className="flex flex-row xs:flex-row justify-between items-center w-full mt-3 border-b pb-7 sm:px-4 px-2 border-gray-500 gap-2">
       <Link href="/Home" className="flex space-x-2">
-        <Image
-          alt="header text"
-          src="/bed.svg"
-          className="sm:w-10 sm:h-10 w-9 h-9"
-          width={24}
-          height={24}
-        />
-        <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">
-          AlkyneFi
-        </h1>
+        <Image alt="header text" src="/bed.svg" className="sm:w-10 sm:h-10 w-9 h-9" width={24} height={24} />
+        <h1 className="sm:text-3xl text-xl font-bold ml-2 tracking-tight">AlkyneFi</h1>
       </Link>
 
-      <a
-        className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
-        href="https://google.com"
-        target="_blank"
-        rel="noopener noreferrer"
+      <div
+        className="flex max-w-fit items-center justify-center space-x-2 text-white px-5 py-2 text-sm shadow-md font-medium transition"
       >
-        <p>Connect Wallet</p>
-      </a>
+        <radix-connect-button />
+      </div>
     </header>
-  )
+  );
 }
-
